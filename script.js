0e0c83636fa93659a88bb2698dea3136972b9306
ChloeCrozier <chloecrozier@gmail.com>
2024-10-01 20:37:32 -0400

    modular desgin

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// index.js
 const Player = require('./player');
 const PokerGame = require('./pokerGame');
 
-// Setup players
 const players = [
   new Player('Alice', 100),
   new Player('Bob', 100),
   new Player('Charlie', 100)
 ];
 
-// Initialize the poker game
 const game = new PokerGame(players, 1, 2);
 
-// Start the game
 game.play();
